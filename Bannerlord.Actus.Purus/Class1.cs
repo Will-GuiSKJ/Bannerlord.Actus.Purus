b5d3f0095588012cd6dea94087c63bad5f840674
William Rosa <william.rosa@parivedasolutions.com>
2020-05-12 19:47:01 -0500

    Add project files.

---
+++ Bannerlord.Actus.Purus/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bannerlord.Actus.Purus
+{
+    public class Class1
+    {
+    }
+}
